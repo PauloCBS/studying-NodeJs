51d83eb0005b70154b4838a313609a0654335241
Paulo Silva <paulo-cesar-batista@hotmail.com>
2024-01-26 19:11:05 +0000

    the route param method was used to request the id from the url and add it to the response. it is possible to also use deestructuring to make a clean code

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ const app = express();
 const PORT = 3333;
 //porta a qual será feita a requisição.
 
-//testando metodo GET: onde o / representa a rota, (reques, response) representa a função 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+//testando metodo GET: onde o / representa a rota que será acessada no navegador, (reques, response) representa a função 
+app.get("/message/:id", (req, res) => {
+    res.send(`request id ${req.params.id}`);
 });
+//res serve para devolver uma resposta. Podemos usar o req para enviar ou obter informações. 
+
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
 //app escute a porta 3333 e retorna uma mensagem de que o servidor está rodando na porta 3333.
