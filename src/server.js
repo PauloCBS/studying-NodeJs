a4b10257cadc6bc920c211c0fa1621bb2dbc1a56
Paulo Silva <paulo-cesar-batista@hotmail.com>
2024-01-29 09:26:40 +0000

    it was used a json body file in the insomnia. to get the information correctly we had to use  app.use(express.json()) to capture the information from the json file and send it back as a response in the console

--- src/server.js
+++ src/server.js
@@ -3,14 +3,19 @@ const express = require("express");
 
 const app = express();
 //inicializou o express.
+app.use(express.json());
+//app.use is necessary to use the json file inside the body of our insomnia body.
 const PORT = 3333;
 //porta a qual será feita a requisição.
 
 
 app.post("/user", (req, res) => { 
     
-res.send("You have called the POST method");
+const {name, email, password} = req.body;
+//this way we do the request from the json file inside the body of our insomnia body. 
+res.send(`Usuário: ${name}. E-mail: ${email}`);
 //note that the main difference between route params and query params is that no params is necessary. 
+//we can also send the information in different formats eg. json.
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
